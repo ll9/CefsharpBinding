ce53e4e8bc973482fdc66abf9aa73b85c9e57554
ll9 <llwierer@web.de>
2018-09-10 22:17:29 +0200

    Loading static Html

--- CefSharpDemo/Form1.cs
+++ CefSharpDemo/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,36 @@ namespace CefSharpDemo
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("");
+            chromeBrowser.LoadHtml(Properties.Resources.index);
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
         }
     }
 }
